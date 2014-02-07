670416f9dc920de4b456fb2776ac17727d3f6f63
Carlos Alexander Vializ <cvializ@vt.edu>
2014-02-07 10:43:16 -0500

    Add imagesLoaded so Masonry lays-out correctly.

--- js/main.js
+++ js/main.js
@@ -4,16 +4,20 @@ require.config({
 		text: 'vendor/require/text',
 		json: 'vendor/require/json',
 		knockout: 'vendor/knockout/knockout-3.0.0',
-		masonry: 'vendor/masonry/masonry.pkgd.min'
+		masonry: 'vendor/masonry/masonry.pkgd.min',
+		imagesLoaded: 'vendor/masonry/imagesloaded.pkgd.min'
 	}
 });
 
-define(['knockout', 'masonry', 'json!data/portfolio.json', 'domReady!'], function (ko, Masonry, portfolio) {
+define(['knockout', 'masonry', 'imagesLoaded', 'json!data/portfolio.json', 'domReady!'], function (ko, Masonry, imagesLoaded, portfolio) {
 	ko.applyBindings(portfolio);
 	var container = document.querySelector('#portfolio');
 	var msnry = new Masonry(container, {
 	  // options
 	  itemSelector: '.portfolio-item'
 	});
-	msnry.layout();
+	
+	imagesLoaded(container, function () {
+		msnry.layout();
+	});
 });
