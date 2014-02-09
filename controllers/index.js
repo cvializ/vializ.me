dd64e589145c342d3fbb67fe085d3099659d6a1b
cvializ <cv@vt.edu>
2014-02-08 23:26:22 -0500

    Rebase with master to get the previous style and cleanup updates. Mesh the branches.

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,10 @@ module.exports = {};
 module.exports.configure = function (app) {
   app.get('/', function (req, res) {
     res.render('base.html', {
-            // uncomment to enable caching for this template
-            //cache: true, 
-            partials: {
-              body: 'index'
-            },
-            title: 'Live. Local. Blacksburg. Events around Blacksburg, by DBI.',
-            events: ''//$eventsNode.html()
-        });
+      partials: {
+        body: 'index'
+      },
+      events: ''//$eventsNode.html()
+    });
   });
 };
--- public/js/PortfolioViewModel.js
+++
@@ -1,9 +0,0 @@
-// Main viewmodel class
-define(['knockout'], function(ko) {
-    return function PortfolioViewModel() {
-        this.firstName = ko.observable('Bert');
-        this.firstNameCaps = ko.computed(function() {
-            return this.firstName().toUpperCase();
-        }, this);
-    };
-});
