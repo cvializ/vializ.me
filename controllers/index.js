74d1a611fb0598694b514ff2bba18672bee8e767
cvializ <cv@vt.edu>
2014-02-09 00:11:59 -0500

    Move away from knockout in favor of server-side templates.

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,33 @@
-module.exports = {};
-module.exports.configure = function (app) {
-  app.get('/', function (req, res) {
-    res.render('base.html', {
-      partials: {
-        body: 'index'
-      },
-      events: ''//$eventsNode.html()
-    });
+var fs = require('fs'),
+    path = require('path'),
+    portfolioPath = path.join(__dirname, '../public/js/data/portfolio.json');
+
+function getData(cb) {
+  fs.readFile(portfolioPath, 'utf8', function (err, data) {
+    if (err) {
+      console.log('Error: ' + err);
+      cb(err);
+    }
+
+    return cb(null, JSON.parse(data));
   });
+}
+
+module.exports = {
+  configure: function (app) {
+    app.get('/', function (req, res) {
+      getData(function (err, data) {
+        if (err) {
+          res.send(500);
+        }
+
+        res.render('base.html', {
+          partials: {
+            body: 'index'
+          },
+          portfolio: data.portfolio
+        });
+      });
+    });
+  }
 };
--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,13 @@ require.config({
     domReady: 'vendor/require/domReady',
     text: 'vendor/require/text',
     json: 'vendor/require/json',
-    knockout: 'vendor/knockout/knockout-3.0.0',
     masonry: 'vendor/masonry/masonry.pkgd.min',
     imagesLoaded: 'vendor/masonry/imagesloaded.pkgd.min'
   }
 });
 
-define(['knockout', 'masonry', 'imagesLoaded', 'json!data/portfolio.json', 'domReady!'],
-function (ko, Masonry, imagesLoaded, portfolio) {
-  ko.applyBindings(portfolio);
+define(['masonry', 'imagesLoaded', 'json!data/portfolio.json', 'domReady!'],
+function (Masonry, imagesLoaded, portfolio) {
   var container = document.querySelector('#portfolio');
   
   var children = container.children;
@@ -24,7 +22,6 @@ function (ko, Masonry, imagesLoaded, portfolio) {
   }
 
   var msnry = new Masonry(container, {
-    // options
     itemSelector: '.portfolio-item',
     columnWidth: '.portfolio-item-masonry'
   });
